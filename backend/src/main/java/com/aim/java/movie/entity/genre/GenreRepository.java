1d51ee2f99c3e9a03e20ffaa41d4c48b475fb922
nicolerenene <64162382+nicolerenene@users.noreply.github.com>
2021-02-10 15:46:42 -0600

    commit

---
+++ backend/src/main/java/com/aim/java/movie/entity/genre/GenreRepository.java
@@ -0,0 +1,7 @@
+package com.aim.java.movie.entity.genre;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenreRepository extends CrudRepository<Genre, Integer> {
+    
+}
